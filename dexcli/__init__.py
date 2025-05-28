aa67f947fbede760449e23c3a63d638c0c2f9019
darrwalk <62713867+darrwalk@users.noreply.github.com>
2025-05-28 16:27:21 +0200

    Add dexcli package init file

---
+++ dexcli/__init__.py
@@ -0,0 +1,11 @@
+"""
+dexcli - DEX Command Line Interface
+"""
+
+__version__ = "0.1.0"
+__author__ = "Your Name"
+__email__ = "your.email@example.com"
+
+from .cli import DEXCLIClient
+
+__all__ = ["DEXCLIClient"]
